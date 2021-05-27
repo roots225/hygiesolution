fc81e01f3b388270ce885f82a9842c917fc7a2a0
Gilles Yao <gillesyao12@gmail.com>
2021-05-27 14:04:37 +0200

    update: add scroll to top and add somes features

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function(){
   $(window).on('scroll load', function() {
 		if ($(".navbar").offset().top > 60) {
 			$(".fixed-top").addClass("top-nav-collapse");
@@ -9,12 +9,22 @@
     
 	// jQuery for page scrolling feature - requires jQuery Easing plugin
 	$(function() {
-		$(document).on('click', 'a.page-scroll', function(event) {
+		$('.page-scroll').on('click', function(event) {
+			event.preventDefault();
 			var $anchor = $(this);
 			$('html, body').stop().animate({
 				scrollTop: $($anchor.attr('href')).offset().top
-			}, 600, 'easeInOutExpo');
-			event.preventDefault();
+			}, 500);
+			
 		});
   });
-// })
+
+	let amountScrolled = 400;
+	$(window).scroll(function() {
+			if ($(window).scrollTop() > amountScrolled) {
+					$('a.back-to-top').fadeIn('500');
+			} else {
+					$('a.back-to-top').fadeOut('500');
+			}
+	});
+})
